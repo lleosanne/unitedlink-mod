515179508a212f43c69ae685bd14d8125d6bfb5e
lleosanne <lisaqut3@gmail.com>
2026-06-16 21:48:18 +0200

    Update UnitedLinkMod.java

--- src/main/java/com/unitedlink/unitedlink/UnitedLinkMod.java
+++ src/main/java/com/unitedlink/unitedlink/UnitedLinkMod.java
@@ -1,20 +1,17 @@
 package com.unitedlink.unitedlink;
-
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
-
 @Mod(UnitedLinkMod.MOD_ID)
 public class UnitedLinkMod {
-
     public static final String MOD_ID = "unitedlink";
     public static final Logger LOGGER = LogUtils.getLogger();
-
     public UnitedLinkMod(IEventBus modEventBus) {
         LOGGER.info("[UnitedLink] Initializing UnitedLink mod v1.0.0");
         NeoForge.EVENT_BUS.register(new UnitedLinkCommandHandler());
+        NeoForge.EVENT_BUS.register(new UnitedLinkLoginHandler());
         LOGGER.info("[UnitedLink] Command handler registered.");
     }
 }
